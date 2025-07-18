3d28de1ea5668eb298e9879f1d26b60de74a1b28
osvaldocs <pablo_campos92@hotmail.com.ar>
2025-07-18 00:50:48 -0500

    terminé el html estatico con css, y agregué algunas funciones

---
+++ javascript/routes.js
@@ -0,0 +1,9 @@
+export const routes = {
+    "/" : "./javascript/views/inicio.html",
+    "catalogo" : "./javascript/views/catalogo.html",
+    "funciona" : "./javascript/views/como-funciona.html"
+}
+
+export async function navigate(pathnameWithQuery) {
+
+}
--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 import {get, post, update, deletes} from './javascript/services';
+import { routes, navigate } from './javascript/routes';
+
+export function setupDataLink(navigateFn) {
+  document.body.addEventListener("click", (e) => {
+    const link = e.target.closest("[data-link]");
+    if (link) {
+      e.preventDefault();
+      const href = link.getAttribute("href");
+      if (href) navigateFn(href);
+    }
+  });
+}
