8bf1ac939a50cc4c2770169c8fa56a689147e99c
osvaldocs <100969798+osvaldocs@users.noreply.github.com>
2025-07-18 12:55:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 import {get, post, update, deletes} from './javascript/services';
 import { routes, navigate } from './javascript/routes';
+import { setupLogin } from './javascript/loginLogout';
 
-export function setupDataLink(navigateFn) {
-  document.body.addEventListener("click", (e) => {
-    const link = e.target.closest("[data-link]");
-    if (link) {
-      e.preventDefault();
-      const href = link.getAttribute("href");
-      if (href) navigateFn(href);
-    }
-  });
-}
+document.body.addEventListener("click", (e) => {  
+  if (e.target.matches("[data-link]")) {
+    e.preventDefault();
+    navigate(e.target.getAttribute("href"));
+  }
+});
+
+
+window.addEventListener("load", () => {
+  navigate(window.location.pathname);
+});
